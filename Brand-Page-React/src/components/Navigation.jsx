403d4280a84a4e7d9bbfe313b4cca2d30bfd076f
Yogesh <yogeshzade999@gmail.com>
2024-03-13 23:52:49 +0530

    develop ui as per the figma in desktop

---
+++ Brand-Page-React/src/components/Navigation.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Navigation = () =>{
+    return(
+        <nav className='container'>
+        <div className="logo">
+          <img src="images/brand_logo.png" alt="brand logo" />
+        </div>
+        <ul>
+          <li><a href="#">Menu</a></li>
+          <li><a href="#">Location</a></li>
+          <li><a href="#">About</a></li>
+          <li><a href="#">Contact</a></li>
+        </ul>
+        <button>Login</button>
+      </nav>
+    )
+}
+
+export default Navigation;
