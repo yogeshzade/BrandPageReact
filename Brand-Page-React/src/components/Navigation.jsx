409c56d7f7afa47be62db37c401352a24c2d1e9c
Yogesh <yogeshzade999@gmail.com>
2024-03-14 13:45:23 +0530

    Mobile responsive

--- Brand-Page-React/src/components/Navigation.jsx
+++ Brand-Page-React/src/components/Navigation.jsx
@@ -1,20 +1,36 @@
-import React from 'react'
+import React from "react";
 
-const Navigation = () =>{
-    return(
-        <nav className='container'>
-        <div className="logo">
-          <img src="images/brand_logo.png" alt="brand logo" />
-        </div>
-        <ul>
-          <li><a href="#">Menu</a></li>
-          <li><a href="#">Location</a></li>
-          <li><a href="#">About</a></li>
-          <li><a href="#">Contact</a></li>
-        </ul>
-        <button>Login</button>
-      </nav>
-    )
-}
+const Navigation = () => {
+  return (
+    <nav className="container nav">
+    <input type="checkbox" id="nav-check"></input>
+      <div class="nav-btn">
+        <label for="nav-check">
+          <span></span>
+          <span></span>
+          <span></span>
+        </label>
+      </div>
+      <div className="logo">
+        <img src="images/brand_logo.png" alt="brand logo" />
+      </div>
+      <ul className="nav-list">
+        <li>
+          <a href="#">Menu</a>
+        </li>
+        <li>
+          <a href="#">Location</a>
+        </li>
+        <li>
+          <a href="#">About</a>
+        </li>
+        <li>
+          <a href="#">Contact</a>
+        </li>
+      </ul>
+      <button>Login</button>
+    </nav>
+  );
+};
 
 export default Navigation;
